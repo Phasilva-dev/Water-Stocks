0a60bf66e0f9ad4b3b20910abfbdb162d7379182
Pedro Henrique <phasilva.work@gmail.com>
2025-09-27 19:13:25 -0300

    Controller praticamente finalizado

--- backend/internal/controller/controller.go
+++ backend/internal/controller/controller.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"log"
 	"simulation/internal/entities"
 	"math/rand/v2"
@@ -12,6 +11,7 @@ import (
 
 func RunSimulation(size, day, toiletType, showerType int) {
 
+	
 	
 	//Iniciando Simulação
 	rng := rand.New(rand.NewPCG(uint64(time.Now().UnixNano()), 0))
@@ -24,44 +24,51 @@ func RunSimulation(size, day, toiletType, showerType int) {
 	
 	//Variaveis de dados
 	populationData := accumulator.NewPopulationData(houses)
+	pulseData := accumulator.NewPulseHouse(0,accumulator.OrderedDeviceKeys())
+
+	/*
 	dailyUsagesDataWindow := make(map[uint8]*accumulator.AccumulatorDay)
 	dailyPulseDataWindow := make(map[uint8]*accumulator.PulseHouse)
+
 
 	for i := uint8(0); i < uint8(day+2); i++ {
 		dailyUsagesDataWindow[uint8(i)] = accumulator.NewAccumulatorDay(uint8(i),accumulator.OrderedDeviceKeys())
 		dailyPulseDataWindow[uint8(i)] = accumulator.NewPulseHouse(uint8(i),accumulator.OrderedDeviceKeys())
-	}
+	}*/
 	
 	for i := uint8(0); i < uint8(day); i++ { // i = day
 		for j := 0; j < size; j++ { // j = house
 			if err := houses[j].GenerateLogs(i+1, rng); err != nil {
 				log.Fatalf("Erro ao gerar logs da casa %d no dia %d: %v", j, i, err)
 			}
-			dailyUsagesDataWindow[1].UpdateAccumulator(i+1,houses[j],dailyUsagesDataWindow)
-			dailyPulseDataWindow[1].UpdatePulseWithWindow(i+1, houses[j],dailyPulseDataWindow)
+			pulseData.UpdatePulse(houses[j])
+			//dailyUsagesDataWindow[1].UpdateAccumulator(i+1,houses[j],dailyUsagesDataWindow)
+			//dailyPulseDataWindow[1].UpdatePulseWithWindow(i+1, houses[j],dailyPulseDataWindow)
 		}
 	}
 
-	
-	fmt.Println("Passou")
-
+	var err error
 	populationData.ViewPopulationData()
-	fmt.Println()
+	pulseData.PrintUsageStatistics()
 
+	/*
 	accumulator.PrintUsagesOverview(dailyUsagesDataWindow, accumulator.OrderedDeviceKeys())
-
 	for k := uint8(1); k < uint8(day+2); k++ {
 		dailyUsagesDataWindow[uint8(k)].RoundAccumulatorDayValues()
 		fmt.Println("Consumo do dia ",k)
 		dailyUsagesDataWindow[uint8(k)].PrintDailyTotals()
-		//dailyUsagesDataWindow[uint8(k)].PrintHourlyWaterConsumption()
-	}
+		dailyPulseDataWindow[k].PrintUsageStatistics()
+		dailyUsagesDataWindow[uint8(k)].PrintHourlyWaterConsumption()
+	}*/
 
-	err := dailyUsagesDataWindow[1].ExportToExcel("consumo_diario.xlsx")
+
+	/*
+	err = dailyUsagesDataWindow[1].ExportToExcel("consumo_diario.xlsx")
 		if err != nil {
 		log.Fatalf("Erro exportando Excel: %v", err)
-	}
-	err = dailyPulseDataWindow[1].ExportPulsesToCSV("pulsos_retangulares.csv")
+	}*/
+
+	err = pulseData.ExportPulsesToCSV("pulsos_retangulares.csv")
 		if err != nil {
 		log.Fatalf("Erro exportando csv: %v", err)
 	}
